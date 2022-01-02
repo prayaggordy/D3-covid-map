9d98becc2694ff525c3b26617000bfb03e7157f7
Prayag Gordy <33771908+prayaggordy@users.noreply.github.com>
2022-01-02 22:54:27 +0530

    delete unused files

--- model data.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-
-statewide <- read.csv("data/md_statewide.csv")
-recovered <- read.csv("data/md_isolation.csv") %>% 
-  mutate(recovered = count - lag(count, default = first(count)))
-
-statewide <- full_join(statewide, recovered) %>% 
-  select(!"count")
